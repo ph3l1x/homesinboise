ba10a3ab3f17b6d8c00e428f5ba54fce6ce412e5
ph3l1x <rperry@mindimage.net>
2016-01-26 23:31:22 -0700

    updated and changed a bunch of items on the website

--- sites/all/themes/homesinboise/template.php
+++ sites/all/themes/homesinboise/template.php
@@ -4,6 +4,45 @@
 //    kpr($variables);
 //}
 
+function homesinboise_preprocess_views_view_fields(&$vars) {
+//    $query = db_query(
+//        'SELECT dl.rets_id, dl.id ' .
+//        'FROM drealty_listing as dl ' .
+//        'LEFT JOIN field_data_field_mls_list_price as fp ' .
+//        'ON dl.id = fp.entity_id ' .
+//        'LEFT JOIN field_data_field_la1_agentid as aid ' .
+//        'ON dl.id = aid.entity_id ' .
+//        'LEFT JOIN field_data_field_mls_list_price as lp ' .
+//        'ON dl.id = fp.entity_id ' .
+//        'WHERE lp.field_mls_list_price_value BETWEEN 200000 AND 209000 ' .
+//        'LIMIT 1, 10'
+
+
+//        'SELECT dl.id, DISTINCT dl.rets_id, fmi.field_mls_main_image_fid ' .
+//        'FROM drealty_listing as dl ' .
+//        'JOIN field_data_field_mls_list_price as fp ' .
+//        'ON dl.id = fp.entity_id ' .
+//        'JOIN field_data_field_mls_main_image as fmi ' .
+//        'ON dl.id = fmi.entity_id ' .
+//        'WHERE fp.field_mls_list_price_value BETWEEN 100000 AND 109000 ' .
+//   //     'GROUP BY dl.rets_id ' .
+//            //'AND'
+//        'LIMIT 1,10 '
+
+//    );
+
+//    $query = db_select('drealty_listing', 'dl')
+//      ->fields('dl', array('id', 'rets_id'))
+//
+//      ->leftJoin('field_data_field_mls_list_price', 'fp', 'dl.id = fp.entity_id')
+////      ->condition('fp.field_mls_list_price_value', array('100000','101000'), 'BETWEEN');
+//      ->range(1,10)
+//      ->execute();
+  //  var_dump($query);
+ //   $vars['shitballs'] = $query->fetchAllKeyed();
+ //   $vars['shitballs'] = 'xXxXXxXXxxxxxxxxxxxxxxx';
+}
+
 function homesinboise_theme($existing, $type, $path)
 {
     $themes['drealty_listing'] = array(
@@ -16,3 +55,4 @@ function homesinboise_theme($existing, $type, $path)
     );
     return $themes;
 }
+
